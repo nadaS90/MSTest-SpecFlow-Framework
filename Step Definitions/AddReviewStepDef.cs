0dc4f5ada5425236601429055e570e90605aa93e
nadaS90 <engnada_45@outlook.com>
2022-10-08 14:25:45 +0200

    Modify add review code

--- Step Definitions/AddReviewStepDef.cs
+++ Step Definitions/AddReviewStepDef.cs
@@ -12,44 +12,21 @@ namespace SpecFlowBasics
 
     public class AddReviewStepDef
     {
-        RegisterPage _registerObject;
         CommonLocators _commonObject;
-        SearchProductPage _searchObject;
         ProductDetailsPage _detailsObject;
         AddReviewPage _reviewObject;
 
 
 
-        static string RegisteredEmail = "";
-
         public AddReviewStepDef(IWebDriver driver)
         {
-            this._registerObject = new RegisterPage(Hooks.driver);
             this._commonObject = new CommonLocators(Hooks.driver);
-            this._searchObject = new SearchProductPage(Hooks.driver);
             this._detailsObject = new ProductDetailsPage(Hooks.driver);
             this._reviewObject = new AddReviewPage(Hooks.driver);
 
 
         }
 
-        [Given(@"User Register to the system")]
-        public void GivenUserRegisterToTheSystem()
-        {
-            _commonObject.ClickRegisterLink();
-            _registerObject.UserSelectGender();
-            _registerObject.UserAddFandLname(Constants.FName, Constants.LName);
-            RegisteredEmail = Constants.Email;
-            _registerObject.UserAddEmail(RegisteredEmail);
-            _registerObject.UserAddPassword(Constants.Password);
-            _registerObject.UserClickRegisterBrn();
-        }
-
-        [Then(@"User search for a product")]
-        public void ThenUserSearchForAProduct()
-        {
-            _searchObject.productSearchWithAutoComplete(Constants.ProductPartName);
-        }
 
         [When(@"User add review")]
         public void WhenUserAddReview()
